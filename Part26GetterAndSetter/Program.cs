459af89e2e898b99fe493fb415583406bb967b83
priyasonar1207 <83229578+priyasonar1207@users.noreply.github.com>
2021-10-06 16:25:15 +0530

    Add project files.

---
+++ Part26GetterAndSetter/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Part26GetterAndSetter
+{
+    class Students
+    {
+        private int _rollno;
+        private string _name;
+        private int _passmark=35;
+        public void StudentDetails()
+        {
+            Console.WriteLine(" Roll no={0} \n Name={1} \n Passmark ={2}", _rollno, _name, _passmark);
+        }
+        public void SetRollNo(int RollNumber)
+        {
+            if(RollNumber<0)
+            {
+                throw new Exception("roll no can not be null");
+
+            }
+            this._rollno = RollNumber;
+        }
+
+        public int GetRollNo()
+        {
+            return _rollno;
+        }
+        public void SetName(string name)
+        {
+            if(string.IsNullOrEmpty(name))
+            {
+                throw new Exception("Name Can not be empty");
+            }
+            this._name = name;
+        }
+        public string GetName()
+        {
+            return this._name;
+        }
+        public int GetPassmark()
+        {
+            return _passmark; ;
+        }
+    }
+  
+
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Students s = new Students();
+            s.SetRollNo(33);
+            s.GetRollNo();
+            s.SetName("Shubham");
+            s.GetName();
+            s.GetPassmark();
+            s.StudentDetails();
+        }
+    }
+}
